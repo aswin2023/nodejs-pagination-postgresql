4cebfcacbc50ea1caffd1ea71d52b21656024b8b
Ashwani <vcsashwani@gmail.com>
2024-01-11 11:19:33 +0530

    add pagination

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -2,12 +2,27 @@ const db = require("../models");
 const Tutorial = db.tutorials;
 const Op = db.Sequelize.Op;
 
+const getPagination = (page, size) => {
+  const limit = size ? +size : 3;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+  const { count: totalItems, rows: tutorials } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, tutorials, totalPages, currentPage };
+};
+
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
     res.status(400).send({
-      message: "Content can not be empty!",
+      message: "Content can not be empty!"
     });
     return;
   }
@@ -16,35 +31,38 @@ exports.create = (req, res) => {
   const tutorial = {
     title: req.body.title,
     description: req.body.description,
-    published: req.body.published ? req.body.published : false,
+    published: req.body.published ? req.body.published : false
   };
 
   // Save Tutorial in the database
   Tutorial.create(tutorial)
-    .then((data) => {
+    .then(data => {
       res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial.",
+          err.message || "Some error occurred while creating the Tutorial."
       });
     });
 };
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  const title = req.query.title;
-  var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
+  const { page, size, title } = req.query;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
-  Tutorial.findAll({ where: condition })
-    .then((data) => {
-      res.send(data);
+  const { limit, offset } = getPagination(page, size);
+
+  Tutorial.findAndCountAll({ where: condition, limit, offset })
+    .then(data => {
+      const response = getPagingData(data, page, limit);
+      res.send(response);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials.",
+          err.message || "Some error occurred while retrieving tutorials."
       });
     });
 };
@@ -54,18 +72,12 @@ exports.findOne = (req, res) => {
   const id = req.params.id;
 
   Tutorial.findByPk(id)
-    .then((data) => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Tutorial with id=${id}.`,
-        });
-      }
+    .then(data => {
+      res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Error retrieving Tutorial with id=" + id,
+        message: "Error retrieving Tutorial with id=" + id
       });
     });
 };
@@ -75,22 +87,22 @@ exports.update = (req, res) => {
   const id = req.params.id;
 
   Tutorial.update(req.body, {
-    where: { id: id },
+    where: { id: id }
   })
-    .then((num) => {
+    .then(num => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was updated successfully.",
+          message: "Tutorial was updated successfully."
         });
       } else {
         res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
         });
       }
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id,
+        message: "Error updating Tutorial with id=" + id
       });
     });
 };
@@ -100,22 +112,22 @@ exports.delete = (req, res) => {
   const id = req.params.id;
 
   Tutorial.destroy({
-    where: { id: id },
+    where: { id: id }
   })
-    .then((num) => {
+    .then(num => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was deleted successfully!",
+          message: "Tutorial was deleted successfully!"
         });
       } else {
         res.send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
         });
       }
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id,
+        message: "Could not delete Tutorial with id=" + id
       });
     });
 };
@@ -124,29 +136,33 @@ exports.delete = (req, res) => {
 exports.deleteAll = (req, res) => {
   Tutorial.destroy({
     where: {},
-    truncate: false,
+    truncate: false
   })
-    .then((nums) => {
+    .then(nums => {
       res.send({ message: `${nums} Tutorials were deleted successfully!` });
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all tutorials.",
+          err.message || "Some error occurred while removing all tutorials."
       });
     });
 };
 
-// Find all published Tutorials
+// find all published Tutorial
 exports.findAllPublished = (req, res) => {
-  Tutorial.findAll({ where: { published: true } })
-    .then((data) => {
-      res.send(data);
+  const { page, size } = req.query;
+  const { limit, offset } = getPagination(page, size);
+
+  Tutorial.findAndCountAll({ where: { published: true }, limit, offset })
+    .then(data => {
+      const response = getPagingData(data, page, limit);
+      res.send(response);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials.",
+          err.message || "Some error occurred while retrieving tutorials."
       });
     });
 };
